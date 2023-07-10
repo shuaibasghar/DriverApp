06b140d26864112c365a4680d197a764f55afc94
Shuaibasghar <shuaibasghargtk@gmail.com>
2023-07-10 20:50:39 +0500

    order tracking screen ui added

--- src/components/screens/OrderTracking/OrderTrakingScreen.tsx
+++ src/components/screens/OrderTracking/OrderTrakingScreen.tsx
@@ -2,6 +2,7 @@ import {View, Text, StyleSheet, Image, TouchableOpacity} from 'react-native';
 import React from 'react';
 
 export default function OrderTrackingScreen() {
+  const handleAccept = () => {};
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -17,7 +18,50 @@ export default function OrderTrackingScreen() {
 
       <View style={styles.body}>
         <View style={styles.innerBody}>
-          <Text>Order Tracking</Text>
+          <Text style={styles.orderTracking}>Order Tracking</Text>
+          <Text style={styles.orderTrackingNumber}>Order Number: 457 034</Text>
+          <View style={styles.checkListContainer}>
+            <Image source={require('../../../assets/images/tickCircle.png')} />
+            <View style={styles.verticalLine} />
+            <Text style={styles.orderAcceptedText}>Order accepted</Text>
+          </View>
+          <View style={styles.checkListContainerTwo}>
+            <Image source={require('../../../assets/images/tickCircle.png')} />
+            <View style={styles.verticalLine} />
+            <Text style={styles.orderPreparingText}>Preparing order</Text>
+          </View>
+          <View style={styles.readyContainer}>
+            <Image
+              source={require('../../../assets/images/tickCircleBlue.png')}
+            />
+            <View style={styles.verticalLine} />
+            <View>
+              <Text style={styles.orderReadyText}>Ready To Collect</Text>
+              <Text style={styles.readyDescription}>
+                Your order was collected and it's
+              </Text>
+              <Text style={styles.readyDescription}>
+                Your order was collected and it's
+              </Text>
+              <Text style={styles.uploadHeadingText}>
+                Upload proof of delivery
+              </Text>
+              <View style={styles.uploadIcon}>
+                <Image source={require('../../../assets/images/arrow.png')} />
+                <Text style={styles.uploadIconText}>Upload</Text>
+              </View>
+            </View>
+          </View>
+          <View style={styles.checkListContainer}>
+            <View style={styles.verticalLine} />
+            <Image
+              source={require('../../../assets/images/orderCollected.png')}
+            />
+            <Text style={styles.orderPreparingText}>Order Collected</Text>
+          </View>
+          <TouchableOpacity style={styles.acceptButton} onPress={handleAccept}>
+            <Text style={styles.acceptButtonText}>Accept</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
@@ -43,9 +87,8 @@ const styles = StyleSheet.create({
   },
   innerBody: {
     flex: 1,
-    alignItems: 'center',
     marginVertical: 40,
-    borderWidth: 1,
+    // borderWidth: 1,
     marginHorizontal: 40,
   },
 
@@ -82,5 +125,100 @@ const styles = StyleSheet.create({
     fontSize: 12,
     color: '#31A702',
     fontFamily: 'Poppins-SemiBold',
+  },
+  verticalLine: {
+    height: '100%',
+    width: 2,
+    backgroundColor: '#F4F6FF',
+    position: 'absolute',
+    left: 20,
+    zIndex: -1,
+  },
+  orderTracking: {
+    textAlign: 'center',
+    fontSize: 20,
+    color: '#3C4253',
+    fontFamily: 'Poppins-SemiBold',
+  },
+  orderTrackingNumber: {
+    textAlign: 'center',
+
+    fontSize: 12,
+    color: '#C0C5D1',
+    fontFamily: 'Poppins-Medium',
+  },
+  checkListContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+    // marginTop: 20,
+  },
+  checkListContainerTwo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 15,
+    marginTop: 20,
+  },
+  orderAcceptedText: {
+    fontSize: 12,
+    color: '#C0C5D1',
+    fontFamily: 'Poppins-Medium',
+  },
+  orderPreparingText: {
+    fontSize: 12,
+    color: '#C0C5D1',
+    fontFamily: 'Poppins-Medium',
+  },
+  readyContainer: {
+    flexDirection: 'row',
+    gap: 15,
+    marginTop: 20,
+  },
+  orderReadyText: {
+    fontSize: 18,
+    color: '#4666FF',
+    fontFamily: 'Poppins-SemiBold',
+  },
+  readyDescription: {
+    fontSize: 12,
+    color: '#C0C5D1',
+    fontFamily: 'Poppins-Medium',
+  },
+  uploadHeadingText: {
+    fontSize: 16,
+    color: '#3C4253',
+    fontFamily: 'Poppins-SemiBold',
+  },
+
+  uploadIcon: {
+    borderWidth: 1,
+    borderStyle: 'dashed',
+    borderColor: '#E3E3E3',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 100,
+    gap: 5,
+  },
+  uploadIconText: {
+    fontSize: 9,
+    fontFamily: 'Poppins-Medium',
+    color: '#3C4253',
+  },
+  acceptButton: {
+    textAlign: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+    backgroundColor: '#4666FF',
+    borderRadius: 8,
+    marginHorizontal: 70,
+
+    paddingVertical: 18,
+  },
+  acceptButtonText: {
+    fontSize: 14,
+    color: '#FFFFFF',
+    fontFamily: 'Poppins-Medium',
   },
 });
